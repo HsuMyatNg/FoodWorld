d2ab58c1e5224aad9301dc8757ed1bbc0f55e1bd
HsuMyatNg <hsumyatnaing2626@gmail.com>
2021-10-18 20:57:50 +0630

    FoodWorld

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function myheart() {
+    document.getElementById('Heart').style.background = "rgb(235, 166, 166)";
+    document.getElementById('order').style.borderColor = "rgb(28, 23, 109)";
+    document.getElementById('order').style.color = "rgb(231, 231, 23)";
+    document.getElementById('icon1').style.color= "red";
+}
+function myheart1() {
+    document.getElementById('Heart1').style.background = "rgb(235, 166, 166)";
+    document.getElementById('order1').style.borderColor = "rgb(28, 23, 109)";
+    document.getElementById('order1').style.color = "rgb(231, 231, 23)";
+    document.getElementById('icon2').style.color= "red";
+}
+function myheart2() {
+    document.getElementById('Heart2').style.background = "rgb(235, 166, 166)";
+    document.getElementById('order2').style.borderColor = "rgb(28, 23, 109)";
+    document.getElementById('order2').style.color = "rgb(231, 231, 23)";
+    document.getElementById('icon3').style.color= "red";
+}
+    
